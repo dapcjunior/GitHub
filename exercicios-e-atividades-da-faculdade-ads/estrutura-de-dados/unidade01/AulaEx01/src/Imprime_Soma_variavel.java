66112da2507fe751dff81dafe88c4387a12ca315
Daniel Pereira <danielpereiracamposjunior@gmail.com>
2022-12-04 13:24:24 -0300

    Exemplo1
    primeiro exemplo da primeira aula

---
+++ exercicios-e-atividades-da-faculdade-ads/estrutura-de-dados/unidade01/AulaEx01/src/Imprime_Soma_variavel.java
@@ -0,0 +1,6 @@
+public class Imprime_Soma_variavel
+{
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ exercicios-e-atividades-da-faculdade-ads/estrutura-de-dados/unidade01/AulaEx01/src/Teste.java
@@ -0,0 +1,14 @@
+public class Teste
+{       // Tipos de variaveis
+    public static void main(String[] args)
+    {
+        char letra = 'A';       // Caractere
+        String nome = "João";   // Cadeia
+        int idade = 33;         // Inteiro
+        float peso = 95.8f;     // Real
+        boolean vivo = true;    // Lógico
+
+        System.out.println("nome, idade, peso " + nome + " " + idade + " " + peso);
+        System.out.println("Voce está vivo? " + vivo);
+    }
+}
