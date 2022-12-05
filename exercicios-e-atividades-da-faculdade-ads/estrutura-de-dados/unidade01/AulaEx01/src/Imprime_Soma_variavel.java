67b071e3285ed558e0b83fff8987a32e867def80
Daniel Pereira <danielpereiracamposjunior@gmail.com>
2022-12-04 21:06:57 -0300

    ex02
    somando  variavel fixa

--- exercicios-e-atividades-da-faculdade-ads/estrutura-de-dados/unidade01/AulaEx01/src/Imprime_Soma_variavel.java
+++ exercicios-e-atividades-da-faculdade-ads/estrutura-de-dados/unidade01/AulaEx01/src/Imprime_Soma_variavel.java
@@ -1,6 +1,11 @@
 public class Imprime_Soma_variavel
 {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args)
+    {
+        int x = 15;
+        int y = 25;
+        int soma = x + y;
+
+        System.out.println("A soma é: " + soma);
     }
 }
