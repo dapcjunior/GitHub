436e1fb1e90cff467b3697d25f8c7410106f3ee1
Daniel Pereira <danielpereiracamposjunior@gmail.com>
2022-12-10 21:56:25 -0300

    tipos de variavel
    Exercicio de tipos de variaveis

---
+++ exercicios-e-atividades-da-faculdade-ads/estrutura-de-dados/unidade01/TipagemDeVariavel/src/Teste.java
@@ -0,0 +1,15 @@
+public class Teste
+{
+    public static void main(String[] args)
+    {
+        char letra = 'A';           //  Caractere
+        String  nome = "Daniel";    //  Cadeia
+        int idade = 46;             //  Inteiro
+        float peso = 95.8f;         //  Real
+        boolean vivo = true;        //  Logico
+
+        System.out.println("Meu nome: " + nome);
+        System.out.println("meu peso: " + peso);
+        System.out.println("voce está vivo " + vivo);
+    }
+}
